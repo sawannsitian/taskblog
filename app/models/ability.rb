e1bafcb24ddb8fc82629fb5222fc304a30a82b70
Neeraj Kumar <neeraj.kumar@payu.in>
2014-11-07 22:46:45 +0530

    editing permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,9 +8,11 @@ class Ability
     elsif user.role == "Data Collection Exec"
       can :read, Tasklist
       can :create, Comment
+      can :edit, Tasklist
     elsif user.role == "Customer Service Exec"
       can :read, Tasklist
       can :create, Tasklist
+      can :edit, Tasklist
     end
   end
 end
