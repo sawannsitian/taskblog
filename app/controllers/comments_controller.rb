080fdedc7a02716b436abfe4aa59e9d9614278a0
sawannsitian <sawannsitian@gmail.com>
2021-08-11 00:35:38 +0530

    testing changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 	
 before_filter :confirm_logged_in
   def create
-    @tasklist = Tasklist.where(id: params[:tasklist_id]).first
+     @tasklist = Tasklist.where(id: params[:tasklist_id]).first
     @comment = @tasklist.comments.create(body: params[:comment][:body], commenter_id: @current_user.try(:id))
     redirect_to tasklist_path(@tasklist)
   end
